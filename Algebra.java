90225654b7335994dce2d62ffc53e6921787e5c7
barzelinger <bar.zelinger@post.runi.ac.il>
2025-11-13 21:58:18 +0200

    Anagram

--- Algebra.java
+++ Algebra.java
@@ -6,22 +6,21 @@
 public class Algebra {
 	public static void main(String args[]) {
 	    // Tests some of the operations
-	    // System.out.println(plus(2,3));   // 2 + 3
-		// System.out.println(minus(2,3));  // 7 - 2
-		// System.out.println(minus(2,7));  // 2 - 7
- 		// System.out.println(times(-4,-2));  // 3 * 4
-		// System.out.println(plus(2,times(4,2)));  // 2 + 4 * 2
-   		// System.out.println(pow(2,0));      // 5^3
-   		// System.out.println(pow(3,5));      // 3^5
-   		// System.out.println(div(0,5));   // 12 / 3    
-   		// System.out.println(div(100,10));    // 5 / 5  
-   		// System.out.println(div(-15,3));   // 25 / 7
-		// System.out.println(div(-15,-3));
-   		// System.out.println(mod(25,7));   // 25 % 7
-   		// System.out.println(mod(120,6));  // 120 % 6    
+	    System.out.println(plus(2,3));   // 2 + 3
+		System.out.println(minus(7,2));  // 7 - 2
+		System.out.println(minus(2,7));  // 2 - 7
+ 		System.out.println(times(3,4));  // 3 * 4
+		System.out.println(plus(2,times(4,2)));  // 2 + 4 * 2
+   		System.out.println(pow(5,3));      // 5^3
+   		System.out.println(pow(3,5));      // 3^5
+   		System.out.println(div(12,3));   // 12 / 3    
+   		System.out.println(div(5,5));    // 5 / 5  
+   		System.out.println(div(25,7));   // 25 / 7
+   		System.out.println(mod(25,7));   // 25 % 7
+   		System.out.println(mod(120,6));  // 120 % 6    
    		System.out.println(sqrt(36));
-	// 	System.out.println(sqrt(263169));
-   	// 	System.out.println(sqrt(76123));
+		System.out.println(sqrt(263169));
+   		System.out.println(sqrt(76123));
 	}  
 
 	// Returns x1 + x2
